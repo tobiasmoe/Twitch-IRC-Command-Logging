bf56570d98b3473d89335108d79dd13021f34207
EC2 Default User <ec2-user@ip-172-31-23-54.us-east-2.compute.internal>
2019-02-11 00:09:08 +0000

    now only logs bot commands for mods

--- main.go
+++ main.go
@@ -1,85 +1,100 @@
 package main
 
 import (
-	"github.com/gempir/go-twitch-irc"
-	"fmt"
-	"strings"
-	"log"
-	"os"
-	"time"
+        "github.com/gempir/go-twitch-irc"
+        "fmt"
+        "strings"
+        "log"
+        "os"
+        "time"
 )
 
 func main() {
-	current_time := time.Now().UTC()
-	file, erro := os.Create(current_time.Format("2006-01-02.txt"))
-	if erro != nil {
-		log.Fatal("cannot create file", erro)
-	}
-	defer file.Close()
-	client := twitch.NewClient("justinfan123123", "oauth:123123123")
-
-	client.OnNewClearchatMessage(func(channel string, user twitch.User, message twitch.Message) {
+        client := twitch.NewClient("MrTobbzi", "123")
+/*
+        client.OnNewClearchatMessage(func(channel string, user twitch.User, message twitch.Message) {
+		current_time := time.Now().UTC()
+        	file, erro := os.Create(current_time.Format("2006-01-02.txt"))
+        	if erro != nil {
+               		log.Fatal("cannot create file", erro)
+        	}
+        	defer file.Close()
 		fmt.Fprintf(file, message.Text)
-		fmt.Fprintf(file, "\n")
-		fmt.Println(message.Text)
-	})
-
-	client.OnNewMessage(func(channel string, user twitch.User, message twitch.Message) {
-		if strings.Contains(message.Text, "!commands") {
+                fmt.Fprintf(file, "\n")
+                fmt.Println(message.Text)
+        })
+*/
+        client.OnNewMessage(func(channel string, user twitch.User, message twitch.Message) {
+	if (len(user.UserType) != 0 || user.Username == "mrtobbzi") {
+		if strings.Contains(message.Text, "!command") {
+                        current_time := time.Now().UTC()
+			if _ := os.Stat(current_time.Format("2006-01-02.txt")) {
+				file, erro := os.Create(current_time.Format("2006-01-02.txt"))
+				if erro != nil {
+				log.Fatal("cannot create file", erro)
+				}
+				defer file.Close()
+			}
 			fmt.Fprintf(file, user.Username)
-			fmt.Fprintf(file, ": ")
-			fmt.Fprintf(file, message.Text)
-			fmt.Fprintf(file, "\n")
-			fmt.Println(message.Text)
-		}
-		if strings.Contains(message.Text, "!addcom") {
-			fmt.Fprintf(file, user.Username)
-			fmt.Fprintf(file, ": ")
-			fmt.Fprintf(file, message.Text)
-                        fmt.Fprintf(file, "\n")
-			fmt.Println(message.Text)
-		}
-		if strings.Contains(message.Text, "!delcom") {
-			fmt.Fprintf(file, user.Username)
-                        fmt.Fprintf(file, ": ")
-			fmt.Fprintf(file, message.Text)
-                        fmt.Fprintf(file, "\n")
-			fmt.Println(message.Text)
-		}
-		if strings.Contains(message.Text, "!editcom") {
-			fmt.Fprintf(file, user.Username)
-                        fmt.Fprintf(file, ": ")
-			fmt.Fprintf(file, message.Text)
-                        fmt.Fprintf(file, "\n")
-			fmt.Println(message.Text)
-		}
-		if strings.Contains(message.Text, "!command add") {
-			fmt.Fprintf(file, user.Username)
-                        fmt.Fprintf(file, ": ")
-                        fmt.Fprintf(file, message.Text)
-                        fmt.Fprintf(file, "\n")
-                        fmt.Println(message.Text)
-		}
-		if strings.Contains(message.Text, "!command edit") {
-                        fmt.Fprintf(file, user.Username)
-                        fmt.Fprintf(file, ": ")
+                        fmt.Fprintf(file, ", ")
                         fmt.Fprintf(file, message.Text)
                         fmt.Fprintf(file, "\n")
                         fmt.Println(message.Text)
                 }
-		if strings.Contains(message.Text, "!command delete") {
-                        fmt.Fprintf(file, user.Username)
-                        fmt.Fprintf(file, ": ")
-                        fmt.Fprintf(file, message.Text)
-                        fmt.Fprintf(file, "\n")
-                        fmt.Println(message.Text)
-                }
-	})
-
-	client.Join("MrTobbzi")
-
-	err := client.Connect()
-	if err != nil {
-		panic(err)
 	}
+		/*
+                if strings.Contains(message.Text, "!command add") {
+                        fmt.Fprintf(file, user.Username)
+                        fmt.Fprintf(file, ": ")
+                        fmt.Fprintf(file, message.Text)
+                        fmt.Fprintf(file, "\n")
+                        fmt.Println(message.Text)
+                }
+                if strings.Contains(message.Text, "!command remove") {
+                        fmt.Fprintf(file, user.Username)
+                        fmt.Fprintf(file, ": ")
+                        fmt.Fprintf(file, message.Text)
+                        fmt.Fprintf(file, "\n")
+                        fmt.Println(message.Text)
+                }
+                if strings.Contains(message.Text, "!editcom") {
+                        fmt.Fprintf(file, user.Username)
+                        fmt.Fprintf(file, ": ")
+                        fmt.Fprintf(file, message.Text)
+                        fmt.Fprintf(file, "\n")
+                        fmt.Println(message.Text)
+    }
+                if strings.Contains(message.Text, "!command add") {
+                        fmt.Fprintf(file, user.Username)
+                        fmt.Fprintf(file, ": ")
+                        fmt.Fprintf(file, message.Text)
+                        fmt.Fprintf(file, "\n")
+                        fmt.Println(message.Text)
+                }
+                if strings.Contains(message.Text, "!command edit") {
+                        fmt.Fprintf(file, user.Username)
+                        fmt.Fprintf(file, ": ")
+                        fmt.Fprintf(file, message.Text)
+                        fmt.Fprintf(file, "\n")
+                        fmt.Println(message.Text)
+                }
+                if strings.Contains(message.Text, "!command delete") {
+                        fmt.Fprintf(file, user.Username)
+                        fmt.Fprintf(file, ": ")
+                        fmt.Fprintf(file, message.Text)
+                        fmt.Fprintf(file, "\n")
+                        fmt.Println(message.Text)
+                }
+		*/
+        })
+
+        client.Join("mrtobbzi")
+
+        err := client.Connect()
+        if err != nil {
+                panic(err)
+        }
 }
+
+
+
